5a538917fbbfd5043492529649977ed68a5af9e2
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2026-08-13 20:23:03 +0200

    style: update primary color and remove unused colors

--- lib/app/theme/app_colors.dart
+++ lib/app/theme/app_colors.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 abstract class AppColors {
   static const Color background = Color(0xFFEDEDED);
-  static const Color primary = Color(0xFF30788F);
+  static const Color primary = Color(0xFF00A896);
   static const Color title = Color(0xFF2F3031);
   static const Color time = Color(0xFF212324);
   static const Color onPrimary = Color(0xFFF6FAFB);
-  static  const Color disabled = Color(0xFFB8C2C5);
-static const Color onDisabled = Color(0xFFE8ECEE);
 }
